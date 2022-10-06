99fb8d867c6f472f81c3ad7e955e587b1a107976
Jedidiah Weller <996175+jedihacks@users.noreply.github.com>
2022-10-06 10:07:12 -0400

    Initial commit

---
+++ apps/example-app/src/app/home/home.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+})
+export class HomePageComponent {}
