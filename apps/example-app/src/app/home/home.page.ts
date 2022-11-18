8f50059a52f8c4824e2d49a8ac8fdeeb8a840c23
Fernando del olmo <fer.olmo92@gmail.com>
2022-11-18 18:04:42 +0100

    fix(): revert default phone

--- apps/example-app/src/app/home/home.page.ts
+++ apps/example-app/src/app/home/home.page.ts
@@ -10,7 +10,7 @@ import { DevicesPopoverComponent } from './components/devices-popover/devices-po
     styleUrls: ['home.page.scss'],
 })
 export class HomePageComponent {
-    public devicePhoneSelected = 'iPhone X';
+    public devicePhoneSelected = 'Default Phone';
 
     // eslint-disable-next-line no-empty-function
     public constructor(public popoverController: PopoverController) {}
