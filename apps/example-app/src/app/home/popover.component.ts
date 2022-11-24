6da5a1f07a24073eb57f228b87d6e6daa7031c56
Nacho <ichbur95@gmail.com>
2022-11-24 10:24:08 -0600

    fix: popover should now have clickable elements and return device name on dismiss

--- apps/example-app/src/app/home/popover.component.ts
+++ apps/example-app/src/app/home/popover.component.ts
@@ -5,21 +5,21 @@ import { PopoverController } from '@ionic/angular';
     // eslint-disable-next-line @angular-eslint/component-selector
     selector: 'app-popover',
     template: ` <ion-list>
-        <ion-item (click)="sizeselected(320, 568)">Iphone 5</ion-item>
-        <ion-item (click)="sizeselected(375, 667)">Iphone 7</ion-item>
-        <ion-item (click)="sizeselected(390, 844)">Iphone 12 Pro</ion-item>
-        <ion-item (click)="sizeselected(768, 1024)">iPad Mini</ion-item>
-        <ion-item (click)="sizeselected(393, 851)">Pixel 5</ion-item>
-        <ion-item (click)="sizeselected(412, 906)">Oneplus 8 Pro</ion-item>
-        <ion-item (click)="sizeselected(360, 640)">Samsung Galaxy S6</ion-item>
-        <ion-item (click)="sizeselected(360, 740)">Samsung Galaxy 8+</ion-item>
+        <ion-item button (click)="sizeselected(320, 568, 'Iphone 5')">Iphone 5</ion-item>
+        <ion-item button (click)="sizeselected(375, 667, 'Iphone 7')">Iphone 7</ion-item>
+        <ion-item button (click)="sizeselected(390, 844, 'Iphone 12 Pro')">Iphone 12 Pro</ion-item>
+        <ion-item button (click)="sizeselected(768, 1024, 'iPad Mini')">iPad Mini</ion-item>
+        <ion-item button (click)="sizeselected(393, 851, 'Pixel 5')">Pixel 5</ion-item>
+        <ion-item button (click)="sizeselected(412, 906, 'Oneplus 8 Pro')">Oneplus 8 Pro</ion-item>
+        <ion-item button (click)="sizeselected(360, 640, 'Samsung Galaxy S6')">Samsung Galaxy S6</ion-item>
+        <ion-item button (click)="sizeselected(360, 740, 'Samsung Galaxy 8+')">Samsung Galaxy 8+</ion-item>
     </ion-list>`,
 })
 export class PopoverComponent {
     // eslint-disable-next-line no-empty-function
     public constructor(public popoverController: PopoverController) {}
 
-    public sizeselected(width: number, height: number): void {
-        void this.popoverController.dismiss({ width, height }, 'selected');
+    public sizeselected(width: number, height: number, device: string): void {
+        void this.popoverController.dismiss({ width, height, device }, 'selected');
     }
 }
