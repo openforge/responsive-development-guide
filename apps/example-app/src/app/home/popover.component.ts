5079100966255e7aa9b594247b2932308272ab9c
Nacho <ichbur95@gmail.com>
2022-11-24 15:28:17 -0600

    fix:changed popover component to now display selected device, also returns new selected device on dismiss

--- apps/example-app/src/app/home/popover.component.ts
+++ apps/example-app/src/app/home/popover.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
     selector: 'app-popover',
     template: ` <ion-list>
-        <ion-item button (click)="sizeselected(320, 568, 'Iphone 5')">Iphone 5</ion-item>
-        <ion-item button (click)="sizeselected(375, 667, 'Iphone 7')">Iphone 7</ion-item>
-        <ion-item button (click)="sizeselected(390, 844, 'Iphone 12 Pro')">Iphone 12 Pro</ion-item>
-        <ion-item button (click)="sizeselected(768, 1024, 'iPad Mini')">iPad Mini</ion-item>
-        <ion-item button (click)="sizeselected(393, 851, 'Pixel 5')">Pixel 5</ion-item>
-        <ion-item button (click)="sizeselected(412, 906, 'Oneplus 8 Pro')">Oneplus 8 Pro</ion-item>
-        <ion-item button (click)="sizeselected(360, 640, 'Samsung Galaxy S6')">Samsung Galaxy S6</ion-item>
-        <ion-item button (click)="sizeselected(360, 740, 'Samsung Galaxy 8+')">Samsung Galaxy 8+</ion-item>
+        <ion-item button (click)="sizeselected(320, 568, 'Iphone 5')">Iphone 5 <ion-icon *ngIf="currentDevice === 'Iphone 5'" slot="end" name="checkmark-outline"></ion-icon></ion-item>
+        <ion-item button (click)="sizeselected(375, 667, 'Iphone 7')">Iphone 7 <ion-icon *ngIf="currentDevice === 'Iphone 7'" slot="end" name="checkmark-outline"></ion-icon></ion-item>
+        <ion-item button (click)="sizeselected(390, 844, 'Iphone 12 Pro')">Iphone 12 Pro <ion-icon *ngIf="currentDevice === 'Iphone 12 Pro'" slot="end" name="checkmark-outline"></ion-icon></ion-item>
+        <ion-item button (click)="sizeselected(393, 851, 'Pixel 5')">Pixel 5 <ion-icon *ngIf="currentDevice === 'Pixel 5'" slot="end" name="checkmark-outline"></ion-icon></ion-item>
+        <ion-item button (click)="sizeselected(412, 906, 'Oneplus 8 Pro')">Oneplus 8 Pro <ion-icon *ngIf="currentDevice === 'Oneplus 8 Pro'" slot="end" name="checkmark-outline"></ion-icon></ion-item>
+        <ion-item button (click)="sizeselected(360, 640, 'Samsung Galaxy S6')"
+            >Samsung Galaxy S6 <ion-icon *ngIf="currentDevice === 'Samsung Galaxy S6'" slot="end" name="checkmark-outline"></ion-icon
+        ></ion-item>
+        <ion-item button (click)="sizeselected(360, 740, 'Samsung Galaxy 8+')"
+            >Samsung Galaxy 8+ <ion-icon *ngIf="currentDevice === 'Samsung Galaxy 8+'" slot="end" name="checkmark-outline"></ion-icon
+        ></ion-item>
     </ion-list>`,
 })
 export class PopoverComponent {
+    @Input() public currentDevice = '';
     // eslint-disable-next-line no-empty-function
     public constructor(public popoverController: PopoverController) {}
 
