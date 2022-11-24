28afc4181bd9cf22db9ca4f4fd496d9956801e0a
Nacho <ichbur95@gmail.com>
2022-11-24 15:44:12 -0600

    fix: disabled eslint unsafe return for e2e app

--- apps/example-app-e2e/src/support/app.po.ts
+++ apps/example-app-e2e/src/support/app.po.ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-unsafe-return,  @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 export const getGreeting = () => cy.get('h1');
