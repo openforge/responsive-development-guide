9798ce88a917aa6e527e4e937f9c7ccb431d7c08
Nacho <ichbur95@gmail.com>
2022-11-24 15:30:28 -0600

    fix: now determines tooltip message on click, assigns device selected on popover dismiss, sets Iphone 7 as default

--- apps/example-app/src/app/home/home.page.ts
+++ apps/example-app/src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 
 import { PopoverComponent } from './popover.component';
+import { TooltipComponent } from './tooltip.component';
 
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
@@ -10,6 +11,8 @@ import { PopoverComponent } from './popover.component';
     styleUrls: ['home.page.scss'],
 })
 export class HomePageComponent {
+    public deviceSelected = 'Iphone 7';
+
     // eslint-disable-next-line no-empty-function
     public constructor(public popoverController: PopoverController) {}
 
@@ -32,15 +35,50 @@ export class HomePageComponent {
         document.documentElement.style.setProperty('--cards-components-font-size', `${value}rem`);
     }
 
+    /*
+     *
+     *  Presents the tooltip displaying information regarding the selected slider icon
+     *
+     */
+    public async presentTooltip(e: Event, type: string): Promise<void> {
+        let tooltipText = '';
+        switch (type) {
+            case 'Document':
+                tooltipText = 'Modifies the px value for font sizes used on the document. Affects the contents used throughout the whole document.';
+                break;
+            case 'Header':
+                tooltipText = 'Modifies the rem value of the header font size';
+                break;
+            case 'Content':
+                tooltipText = 'Modifies the rem value for all items inside of the content tag.';
+                break;
+            case 'Footer':
+                tooltipText = 'Modifies the rem value of the footer font size';
+                break;
+        }
+        const popover = await this.popoverController.create({
+            component: TooltipComponent,
+            event: e,
+            componentProps: {
+                tooltipText,
+            },
+        });
+        await popover.present();
+    }
+
     public async presentPopover(e: Event): Promise<void> {
         const popover = await this.popoverController.create({
             component: PopoverComponent,
             event: e,
+            componentProps: {
+                currentDevice: this.deviceSelected,
+            },
         });
 
         void popover.onDidDismiss().then(response => {
             if (response.role === 'selected') {
-                const sizeData = response.data as { height: number; width: number };
+                const sizeData = response.data as { height: number; width: number; device: string };
+                this.deviceSelected = sizeData.device;
                 const nodeList = Array.from(document.getElementsByClassName('phone') as HTMLCollectionOf<HTMLElement>);
                 nodeList.forEach(ele => ele.style.setProperty('--device-height', `${sizeData.height}px`));
                 nodeList.forEach(ele => ele.style.setProperty('--device-width', `${sizeData.width}px`));
