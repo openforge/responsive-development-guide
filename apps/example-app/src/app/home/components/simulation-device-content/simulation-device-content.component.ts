e194ab30bfdd07fd5d684303aa9bfd9a6f88666c
Jedidiah Weller <jedi@openforge.io>
2022-12-11 14:17:51 -0500

    cleanup():  renaming the app-content component to be more clear and concise. Removed redundant example cards

--- apps/example-app/src/app/home/components/simulation-device-content/simulation-device-content.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'openforge-simulation-device-content',
-    templateUrl: 'simulation-device-content.component.html',
-    styleUrls: ['simulation-device-content.component.scss'],
-})
-export class SimulationDeviceContentComponent {}
