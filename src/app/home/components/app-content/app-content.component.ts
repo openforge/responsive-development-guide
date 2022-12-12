5a92c954a09746699565f6c68429c290de23aca1
Fernando del olmo <fer.olmo92@gmail.com>
2022-12-12 18:04:38 +0100

    feat(): update general styles

--- src/app/home/components/app-content/app-content.component.ts
+++ src/app/home/components/app-content/app-content.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'openforge-app-content',
     templateUrl: 'app-content.component.html',
     styleUrls: ['app-content.component.scss'],
 })
-export class AppContentComponent {}
+export class AppContentComponent {
+    @Input() borderRadius: boolean = true;
+}
